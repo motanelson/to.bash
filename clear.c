02bc7945b682e849df18ffce922598308ba6755a
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-09 21:33:17 +0000

    Add files via upload

---
+++ clear.c
@@ -0,0 +1,12 @@
+//gcc -o clear clear.c
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc , char *argv[]){
+    int n=0;
+    int y=64;
+    
+    for (n=1;n<y;n++)printf("\n");
+    
+    
+    return 0;
+}
